31d67757ca4d929fc0a5ed440e9ece61a50cd479
acheney <alexanderwcheney@gmail.com>
2018-12-14 00:07:25 -0800

    remove dupe day1 files

--- python/adventday1_python.py
+++
@@ -1,9 +0,0 @@
-f= open('adventday1_input')
-current_freq=0
-
-for i in f.read().split("\n")[:-1]:
-	if i[0]=='+':
-		current_freq+=int(i[1:])
-	else:
-		current_freq-=int(i[1:])
-print (current_freq)
